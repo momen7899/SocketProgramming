9cf3bc0cdfbddf3c32411a28df631d79a89cb41f
Momen Hamaveysi <momen.hamaveysi@kubakgroup.com>
2021-06-29 14:38:33 +0430

    Complete Project

--- Project/Server/index.py
+++ Project/Server/index.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, flash, request
 from flask_mysqldb import MySQL
 from flask_socketio import SocketIO
 import hashlib
@@ -21,21 +21,42 @@ def login():
 @app.route('/chat/', methods=['POST', 'GET'])
 def chat():
     if request.method == 'GET':
-        return render_template("login.html")
+        return redirect("http://127.0.0.1:5000/")
     elif request.method == 'POST':
         name = request.form['username']
         md = md5(request.form['pass'])
         cursor = mysql.connection.cursor()
-        cursor.execute(
-            ''' SELECT id from user WHERE user_name = %s AND password = %s''', (name, md))
-        mysql.connection.commit()
-        if cursor.rowcount >= 1:
+        if request.form['submit'] == 'login':
+            cursor.execute(
+                ''' SELECT id from user WHERE user_name = %s AND password = %s''', (name, md))
+            mysql.connection.commit()
+            if cursor.rowcount >= 1:
+                cursor.close()
+                name = name + ":"
+                return render_template("index.html", name=name)
             cursor.close()
-            name = name + ":"
-            return render_template("index.html", name=name)
+            flash("There is no such a user")
+            return redirect("http://127.0.0.1:5000/")
+        elif request.form['submit'] == 'sign':
+            try:
+                cursor.execute(
+                    ''' INSERT INTO user  (user_name, password) VALUES (%s, %s)''', (name, md))
+                mysql.connection.commit()
+            except:
+                flash("User Name has been used.\nChoose another one")
+                return redirect("http://127.0.0.1:5000/")
+            if cursor.rowcount >= 1:
+                flash("User Successfully Added")
+            else:
+                flash("Data Error Please Try Again")
+            cursor.close()
+            return redirect("http://127.0.0.1:5000/")
 
-        cursor.close()
-        return "There Is No such a User"
+
+@app.route('/user/register', methods=['POST', 'GET'])
+def register():
+    print(request.form['sign-btn'])
+
 
 def md5(password):
     md5 = hashlib.md5(password.encode()).hexdigest()
